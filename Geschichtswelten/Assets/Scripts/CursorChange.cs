bc47a50aea8baabad24365be9e363e55c027827c
SillesianGuy <jakub.jan.luczak@gmail.com>
2023-01-17 13:17:51 +0100

    Extra Cursor Texture On Button

--- Geschichtswelten/Assets/Scripts/CursorChange.cs
+++ Geschichtswelten/Assets/Scripts/CursorChange.cs
@@ -5,10 +5,20 @@ using UnityEngine;
 public class CursorChange : MonoBehaviour
 {
     public Texture2D cursorTexture;
+    public Texture2D cursorOnButton;
     
     void Start()
     {
         Cursor.SetCursor(cursorTexture, Vector2.zero, CursorMode.ForceSoftware);
     }
 
+    public void CursorOnButtonEnter()
+    {
+        Cursor.SetCursor(cursorOnButton, Vector2.zero, CursorMode.ForceSoftware);
+    }
+
+    public void CursorOnButtonExit()
+    {
+        Cursor.SetCursor(cursorTexture, Vector2.zero, CursorMode.ForceSoftware);
+    }
 }
