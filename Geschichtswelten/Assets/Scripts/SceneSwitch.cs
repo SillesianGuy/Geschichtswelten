c6ffce563029d725c7852ec46d232118ba6b703a
Nicole Scheitler <nicole.scheitler@dinkelbar.de>
2023-01-17 10:46:53 +0100

    add scene switch script

---
+++ Geschichtswelten/Assets/Scripts/SceneSwitch.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitch : MonoBehaviour, IPointerClickHandler
+{
+    [SerializeField] private int sceneID;
+    
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        SceneManager.LoadSceneAsync(sceneID);
+    }
+}
