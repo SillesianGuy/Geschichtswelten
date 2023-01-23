8a6d921ba55731654540b4cc069f99f86f5cec84
Nicole Scheitler <nicole.scheitler@dinkelbar.de>
2023-01-23 13:21:52 +0100

    finish key puzzle

---
+++ Geschichtswelten/Assets/Scripts/FinnsDoor.cs
@@ -0,0 +1,37 @@
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class FinnsDoor : MonoBehaviour, IPointerClickHandler
+{
+    [SerializeField] private GameObject inventory;
+    [SerializeField] private GameObject finnsRoom;
+    [SerializeField] private GameObject gang;
+    
+    private bool _opened = false;
+    
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        Inventory inventoryScript = inventory.GetComponent<Inventory>();
+        
+        if (inventoryScript.selectedItem == null)
+        {
+            return;
+        }
+
+        if (!_opened)
+        {
+            if (inventoryScript.selectedItem.GetComponent<Image>().sprite.name == "SchlüsselFinnsZimmer")
+            {
+                _opened = true;
+                finnsRoom.SetActive(true);
+                gang.SetActive(false);
+                inventoryScript.RemoveItem(inventoryScript.selectedItemId);
+            }
+        } else {
+            finnsRoom.SetActive(true);
+            gang.SetActive(false);
+        }
+    }
+}
