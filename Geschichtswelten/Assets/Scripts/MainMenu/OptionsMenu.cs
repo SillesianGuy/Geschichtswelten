7837c2af295425bfb41367ccac36216da71b0ffd
Nicole Scheitler <nicole.scheitler@dinkelbar.de>
2023-03-18 12:37:39 +0100

    add Language selection in OptionsMenu.cs

--- Geschichtswelten/Assets/Scripts/MainMenu/OptionsMenu.cs
+++ Geschichtswelten/Assets/Scripts/MainMenu/OptionsMenu.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.Audio;
 using UnityEngine.UI;
@@ -9,15 +10,27 @@ public class OptionsMenu : MonoBehaviour
 {
     [SerializeField] private Slider volumeSlider;
     [SerializeField] private AudioMixer mixer;
+    [SerializeField] private TMP_Dropdown dropdown;
 
     private void Start()
     {
         volumeSlider.value = GameData.Instance.CurrentVolume;
+        dropdown.value = (int) GameData.Instance.CurrentLanguage;
     }
 
     public void SetVolume(float volume)
     {
         mixer.SetFloat("Volume", Mathf.Log10(volume) * 20);
         GameData.Instance.CurrentVolume = volume;
+    }
+
+    public void SelectLanguage()
+    {
+        GameData.Instance.CurrentLanguage = dropdown.value switch
+        {
+            0 => GameData.Language.DE,
+            1 => GameData.Language.EN,
+            _ => GameData.Language.EN
+        };
     }
 }
