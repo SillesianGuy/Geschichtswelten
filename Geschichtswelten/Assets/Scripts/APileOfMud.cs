9677e4b2d22e2b558eb4762ac13aa92e2b78c896
Nicole Scheitler <nicole.scheitler@dinkelbar.de>
2023-04-17 11:12:44 +0200

    fix APileOfMud usable for more cases

--- Geschichtswelten/Assets/Scripts/APileOfMud.cs
+++ Geschichtswelten/Assets/Scripts/APileOfMud.cs
@@ -6,8 +6,9 @@ public class APileOfMud : MonoBehaviour, IPointerClickHandler
 {
     [SerializeField] private GameObject inventory;
     [SerializeField] private Sprite aPileOfMudNaked;
-    [SerializeField] private GameObject book;
     [SerializeField] private GameObject dialog;
+    [SerializeField] private GameObject[] enableObjects;
+    [SerializeField] private string itemName;
 
     public void OnPointerClick(PointerEventData eventData)
     {
@@ -18,10 +19,15 @@ public class APileOfMud : MonoBehaviour, IPointerClickHandler
             return;
         }
 
-        if (inventoryScript.selectedItem.GetComponent<Image>().sprite.name == "Schaufel")
+        if (inventoryScript.selectedItem.GetComponent<Image>().sprite.name == itemName)
         {
             gameObject.GetComponent<Image>().sprite = aPileOfMudNaked;
-            book.SetActive(true);
+
+            foreach (var obj in enableObjects)
+            {
+                obj.SetActive(true);
+            }
+            
             inventoryScript.RemoveItem(inventoryScript.selectedItemId);
             if (dialog != null)
             {
